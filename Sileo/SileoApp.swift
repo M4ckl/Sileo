b224aa10cf2ce9d9cde64c5e26c46ed45791e7c0
Michail Romaniuk <104836585+M4ckl@users.noreply.github.com>
2026-01-07 16:57:02 +0100

    Initial Commit

---
+++ Sileo/SileoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SileoApp.swift
+//  Sileo
+//
+//  Created by Misha on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SileoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SileoTests/SileoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SileoTests.swift
+//  SileoTests
+//
+//  Created by Misha on 07/01/2026.
+//
+
+import Testing
+@testable import Sileo
+
+struct SileoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
